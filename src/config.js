6edb10272f335e605f5c3a5ee59eb30d238c4578
heathervv <heathervandervecht@gmail.com>
2021-08-12 11:03:15 -0400

    Add Opencare to work

--- src/config.js
+++ src/config.js
@@ -54,6 +54,15 @@ const contact = {
 
 const work = [
   {
+    title: 'Opencare: Patient Growth',
+    url: '//opencare.com',
+    copy: 'I spent most of my time with Opencare on the Patient Growth team as a tech lead. The mission for this product line was to connect patients to top-tier dentists in their neighbourhood based on their wants/needs. Operating in both Canada and the United States, we built and maintained a marketing site (opencare.com), a patient app, and a practice app. There was *a lot* we managed to build, but I\'m personally most proud of the completely rebuilt patient funnel (search.opencare.com/recommendation) which I built entirely in React to replace the original in AngularJS, and mentoring the team through improving QA processes, app accessibility, and feature scoping.',
+  },
+  {
+    title: 'Opencare: Revenue Cycle Management',
+    copy: 'This was a product line to help practices manage patient insurance before and after their appointments. This was done through an internal product that enabled the operations team to work through cases. When I joined this product line, the relationship between product and operations was strained - the product team was operating under a "ship fast and often" ethos, which, while great, often disrupted the operations team as they had tight timelines and had a low risk threshold for bugs and mid-day releases. So I focused on improving this communication-breakdown by refreshing the bug process and introducing functional reviews with key stakeholders before releases.',
+  },
+  {
     title: 'Telus Digital Life',
     url: '//telus.com/mobility/accessories',
     copy: 'Nascent worked closely with TELUS to innovate the accessory space, and as a part of Nascent I co-lead the team that worked on this project. The main goal we worked towards with TELUS.com/accessories was how to bring the incredible in-store experience they\'d already developed onto the website. It also presented some fantastic technical opportunities with features like real time inventory and a mobility-wide cart.',
