c1f48029533e764c5302dfce8b559a07ac974e43
Heather Vandervecht <heathervandervecht@gmail.com>
2026-02-26 20:06:22 -0500

    chore: base ts setup

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+const js = require('@eslint/js');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
+
+module.exports = [
+  {
+    ignores: ['build/**', 'dist/**', 'node_modules/**', '**/*.d.ts'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        localStorage: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/purity': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+      'react/forbid-prop-types': [1, { forbid: [] }],
+      'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    },
+  },
+];
